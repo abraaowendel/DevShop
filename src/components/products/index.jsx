94bee045163c35284462b7ac12ccb511b60d3cd5
abraaowendel <abraaowendel@protonmail>
2024-09-06 11:52:33 -0300

    update

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -7,7 +7,7 @@ export const Products = ({ props }) => {
         props.map((item, key) => (
           <C.Card key={key}>
               <img src={item.image} alt="" />
-              <h3>TESTE</h3>
+              <h3>{item.name}</h3>
               <p><span>R$ {(item.price * 20  / 100 + item.price).toFixed(2)}</span> R$ {(item.price).toFixed(2)}</p>
               <p>ou 6 x R$ {(item.price / 6).toFixed(2)}</p>
           </C.Card>
--- src/pages/Shirts/index.jsx
+++ src/pages/Shirts/index.jsx
@@ -2,19 +2,58 @@ import { useEffect, useState } from "react";
 import { Loading } from "../../components/loading";
 import { Products } from "../../components/products";
 import * as C from "./styled";
-import ScrollReveal from "scrollreveal";
 
 const Shirts = () => {
 
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([
+    {
+      "name": "Camiseta Básica Preta",
+      "price": 39.99,
+      "image": "https://exemplo.com/camisetas/preta.jpg"
+    },
+    {
+      "name": "Camiseta Estampada Rock",
+      "price": 49.99,
+      "image": "https://exemplo.com/camisetas/rock.jpg"
+    },
+    {
+      "name": "Camiseta Manga Longa Listrada",
+      "price": 59.99,
+      "image": "https://exemplo.com/camisetas/listrada.jpg"
+    },
+    {
+      "name": "Camiseta Oversized Branca",
+      "price": 45.99,
+      "image": "https://exemplo.com/camisetas/branca.jpg"
+    },
+    {
+      "name": "Camiseta com Logo Minimalista",
+      "price": 35.99,
+      "image": "https://exemplo.com/camisetas/minimalista.jpg"
+    },
+    {
+      "name": "Camiseta Regata Fitness",
+      "price": 29.99,
+      "image": "https://exemplo.com/camisetas/regata.jpg"
+    },
+    {
+      "name": "Camiseta Polo Algodão Egípcio",
+      "price": 69.99,
+      "image": "https://lh3.googleusercontent.com/gg/AJIvXivuPje6t5bQfjiUbltvMIIpIZMwBmCP1BNZIwJz_ASPdYGXR8Q00l37eRwzFhtkJroGGMJ_l9fOycdr-8xJDA4sLxghQdg6asmMCTo4rRch_C2vY2NdQYFezVl13mPYuHkLZk8YVyN6n5sjiOYnPRhERKtPUCU6_5oi13ecI2Ftg-NeawQD"
+    },
+    {
+      "name": "Camiseta Tie-Dye Colorida",
+      "price": 49.99,
+      "image": "https://exemplo.com/camisetas/tie-dye.jpg"
+    }
+  ]);
 
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+  /* useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=6")
       .then((res) => res.json())
       .then((json) => setProducts(json));
   }, []);
-
-
+ */
   return (
     <C.Container>
       {!products && <Loading />}
@@ -25,11 +64,11 @@ const Shirts = () => {
           </C.Painel>
           <C.Filter>
             <span>ORDERNAR POR:</span>
-            <select name="" id="">
-              <option value="">Ordem alfabética, A-Z</option>
-              <option value="">Ordem alfabética, Z-A</option>
-              <option value="">Maior preço</option>
-              <option value="">Menor preço</option>
+            <select>
+              <option value="asc">Ordem alfabética, A-Z</option>
+              <option value="desc">Ordem alfabética, Z-A</option>
+              <option value="maior">Maior preço</option>
+              <option value="menor">Menor preço</option>
             </select>
           </C.Filter>
           <Products props={products} />
